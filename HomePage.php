89646f96682f05b5a6c5f0895a8a11bf016b0902
1204848 <j.c.1983@live.com>
2015-11-02 16:11:14 +0000

    a

--- HomePage.php
+++ HomePage.php
@@ -16,7 +16,7 @@ function displayAccessLevelInformation($access_level) {
         echo "<p>You are currently logged in as a standard user</p>";
     }
     elseif ($access_level == "root") {
-        echo "<p>You are currently logged in as a root user</p>";
+        echo "root";
         echo "<p>You now have access to additional administrative features</p>";
     }
 }
