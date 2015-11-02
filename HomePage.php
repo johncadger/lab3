3c2366a822750d5fa1e308c15cd9f29739d2e709
1204848 <j.c.1983@live.com>
2015-11-02 16:15:49 +0000

    a

--- HomePage.php
+++ HomePage.php
@@ -11,12 +11,12 @@ $access_level = $_COOKIE['access_level'];
 echo $_COOKIE['access_level'];
 echo $access_level;
 
-function displayAccessLevelInformation($access_level) {
-    if ($access_level == "standarduser") {
+function displayAccessLevelInformation() {
+    if ($_COOKIE['access_level'] == "standarduser") {
         echo "<p>You are currently logged in as a standard user</p>";
     }
-    elseif ($access_level == "root") {
-        echo "root";
+    elseif ($_COOKIE['access_level'] == "root") {
+        echo "<p>You are currently logged in as a root user</p>";
         echo "<p>You now have access to additional administrative features</p>";
     }
 }
