2fdf4f5123d03d8568ba77f7dcd76b06888b73fc
1204848 <j.c.1983@live.com>
2015-11-02 13:52:27 +0000

    a

--- CheckLogin.php
+++ CheckLogin.php
@@ -16,4 +16,11 @@
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-echo $username . " ". $password;
+if ($username = "a"){
+    if ($password = "b"){
+        setcookie('username','a');
+        setcookie('access_level','standarduser');
+        header('Location: http://1204848-lab1.azurewebsites.net/HomePage.php');
+    }
+}
+
--- HomePage.php
+++ HomePage.php
@@ -5,3 +5,13 @@
  * Date: 02/11/2015
  * Time: 13:22
  */
+
+function displayAccessLevelInformation() {
+    if ($_COOKIE['access_level'] == "standarduser") {
+        echo "<p>You are currently logged in as a standard user</p>";
+    }
+    elseif ($_COOKIE['access_level'] == "root") {
+        echo "<p>You are currently logged in as a root user</p>";
+        echo "<p>You now have access to additional administrative features</p>";
+    }
+}
